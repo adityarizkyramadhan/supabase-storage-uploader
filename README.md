326ca9995138daec92bf15f3784f0fdd192f7ac8
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2023-02-23 08:19:14 +0700

    add documentation

--- README.md
+++ README.md
@@ -9,6 +9,54 @@ Download ekstensi
 go get github.com/adityarizkyramadhan/supabase-storage-uploader
 ```
 
+#Update
+
+Untuk membuat code yang lebih muda dibaca agar dapat dipergunakan lebih simple
+
+
+```go
+package main
+
+import (
+	"os"
+
+	supabasestorageuploader "github.com/adityarizkyramadhan/supabase-storage-uploader"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Buat Client
+	supClient := supabasestorageuploader.NewSupabaseClient(
+		"PROJECT_URL",
+		"PROJECT_API_KEYS",
+		"STORAGE_NAME",
+		"STORAGE_FOLDER",
+	)
+
+	r.POST("/upload/v2", func(c *gin.Context) {
+		file, err := c.FormFile("avatar")
+		if err != nil {
+			c.JSON(400, gin.H{"data": err.Error()})
+			return
+		}
+		link, err := supClient.Upload(file)
+		if err != nil {
+			c.JSON(500, gin.H{"data": err.Error()})
+			return
+		}
+		c.JSON(200, gin.H{"data": link})
+	})
+
+	r.Run(":8080")
+}
+
+```
+
+
+<h6> Contoh fungsi yang usang</h6>
+
 ```go
 package main
 
--- docs/example/main.go
+++ docs/example/main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	r := gin.Default()
 
+	// Deprecated : Fungsi ini contoh supabase storage uploader yang kurang efesian dan kurang clean
 	r.POST("/upload", func(c *gin.Context) {
 		file, err := c.FormFile("avatar")
 		if err != nil {
@@ -17,6 +18,28 @@ func main() {
 			return
 		}
 		link, err := supabasestorageuploader.Upload(os.Getenv("HOST"), os.Getenv("TOKEN"), os.Getenv("STORAGE_NAME"), os.Getenv("STORAGE_PATH"), file)
+		if err != nil {
+			c.JSON(500, gin.H{"data": err.Error()})
+			return
+		}
+		c.JSON(200, gin.H{"data": link})
+	})
+
+	// Buat Client
+	supClient := supabasestorageuploader.NewSupabaseClient(
+		"PROJECT_URL",
+		"PROJECT_API_KEYS",
+		"STORAGE_NAME",
+		"STORAGE_FOLDER",
+	)
+
+	r.POST("/upload/v2", func(c *gin.Context) {
+		file, err := c.FormFile("avatar")
+		if err != nil {
+			c.JSON(400, gin.H{"data": err.Error()})
+			return
+		}
+		link, err := supClient.Upload(file)
 		if err != nil {
 			c.JSON(500, gin.H{"data": err.Error()})
 			return
