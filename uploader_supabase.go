ed8a53c2c02d4955c5e2e94548fb13adc1225031
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2023-08-26 08:02:13 +0700

    add get list object in bucket

--- uploader_supabase.go
+++ uploader_supabase.go
@@ -2,10 +2,12 @@ package supabasestorageuploader
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"log"
 	"mime/multipart"
 	"net/http"
+	"time"
 )
 
 type Client struct {
@@ -21,21 +23,20 @@ func New(
 	token string,
 	bucketName string,
 ) *Client {
-	url := projectUrl + "/storage/v1/object/" + bucketName + "/"
 	fileUrl := projectUrl + "/storage/v1/object/public/" + bucketName + "/"
 	return &Client{
 		token:      token,
 		httpClient: &http.Client{},
 		fileUrl:    fileUrl,
-		urlProject: url,
+		urlProject: projectUrl,
 		bucketName: bucketName,
 	}
 }
 
 func (c *Client) Upload(fileHeader *multipart.FileHeader) (string, error) {
 	if fileHeader == nil {
-		log.Println("Error reading file:", errFileNotFound)
-		return "", errFileNotFound
+		log.Println("Error reading file:", ErrFileNotFound)
+		return "", ErrFileNotFound
 	}
 	file, err := fileHeader.Open()
 	if err != nil {
@@ -59,7 +60,8 @@ func (c *Client) Upload(fileHeader *multipart.FileHeader) (string, error) {
 		log.Println("Error closing multipart writer:", err)
 		return "", err
 	}
-	request, err := http.NewRequest(http.MethodPost, c.urlProject+fileHeader.Filename, &requestBody)
+	url := c.urlProject + "/storage/v1/object/" + c.bucketName + "/" + fileHeader.Filename
+	request, err := http.NewRequest(http.MethodPost, url, &requestBody)
 	if err != nil {
 		log.Println("Error creating request:", err)
 		return "", err
@@ -74,11 +76,73 @@ func (c *Client) Upload(fileHeader *multipart.FileHeader) (string, error) {
 	defer response.Body.Close()
 	if response.StatusCode != http.StatusOK {
 		log.Println("Received non-200 response:", response.StatusCode)
-		return "", err
+		return "", ErrBadRequest
 	}
 	return c.linkFile(fileHeader.Filename), nil
 }
 
 func (c *Client) linkFile(filename string) string {
 	return c.fileUrl + filename
+}
+
+type RequestBodyListBucket struct {
+	Prefix string `json:"prefix"`
+	Limit  int    `json:"limit"`
+	Offset int    `json:"offset"`
+	SortBy struct {
+		Column string `json:"column"`
+		Order  string `json:"order"`
+	} `json:"sortBy"`
+	Search string `json:"search"`
+}
+
+type ResponseListBucket []struct {
+	Name           string    `json:"name"`
+	ID             string    `json:"id"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	CreatedAt      time.Time `json:"created_at"`
+	LastAccessedAt time.Time `json:"last_accessed_at"`
+	Metadata       struct {
+		CacheControl   string    `json:"cacheControl"`
+		ContentLength  int       `json:"contentLength"`
+		ETag           string    `json:"eTag"`
+		HTTPStatusCode int       `json:"httpStatusCode"`
+		LastModified   time.Time `json:"lastModified"`
+		Mimetype       string    `json:"mimetype"`
+		Size           int       `json:"size"`
+	} `json:"metadata"`
+}
+
+func (c *Client) ListBucket(requestBody *RequestBodyListBucket) (*ResponseListBucket, error) {
+	jsonBody, err := json.Marshal(requestBody)
+	if err != nil {
+		log.Println("Error marshaling JSON:", err)
+		return nil, err
+	}
+	url := c.urlProject + "/storage/v1/object/list/" + c.bucketName
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		log.Println("Error creating request:", err)
+		return nil, err
+	}
+	request.Header.Set("Authorization", "Bearer "+c.token)
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := c.httpClient.Do(request)
+	if err != nil {
+		log.Println("Error sending request:", err)
+		return nil, err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		log.Println("Received non-200 response:", response.StatusCode)
+		return nil, err
+	}
+	var responseBody ResponseListBucket
+	err = json.NewDecoder(response.Body).Decode(&responseBody)
+	if err != nil {
+		log.Println("Error decoding response:", err)
+		return nil, err
+	}
+	return &responseBody, nil
 }
